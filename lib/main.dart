8699fb2c0cfecd2d324d14d6f4df726a0383228e
Tasikul Islam <iamtasikul@gmail.com>
2020-12-31 12:35:53 +0530

    Alert Button added

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'quiz_brain.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
 
 QuizBrain quizBrain = new QuizBrain();
 
@@ -35,10 +36,23 @@ class _QuizPageState extends State<QuizPage> {
     bool correctAnswer = quizBrain.getQuestionAnswer();
   
     setState(() {
-    if (userPickedAnswer == correctAnswer) {
-      scoreKeeper.add(Icon(Icons.check, color:Colors.green));
-    } else {
-      scoreKeeper.add(Icon(Icons.close, color:Colors.red));
+      if(quizBrain.isFinished() == true){
+        Alert(
+          context: context,
+          title: 'Finished!',
+          desc: 'You\'ve reached the end of the quiz.',
+        ).show();
+
+        quizBrain.reset();
+
+        scoreKeeper = [];
+      }
+    else{
+      if (userPickedAnswer == correctAnswer) {
+        scoreKeeper.add(Icon(Icons.check, color:Colors.green));
+      } else {
+        scoreKeeper.add(Icon(Icons.close, color:Colors.red));
+      }
     }
 
     
