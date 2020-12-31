79aff6ae49ad2c6e4de29ea2ca58dee3b32b9445
Tasikul Islam <iamtasikul@gmail.com>
2020-12-31 11:43:45 +0530

    Encapsulation in Action

--- lib/main.dart
+++ lib/main.dart
@@ -46,8 +46,6 @@ class _QuizPageState extends State<QuizPage> {
   // Question q1 =
   //     Question(q: 'You can lead a cow down stairs but not up stairs', a: false);
 
-  int questionNumber = 0;
-
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -60,7 +58,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                quizBrain.questionBank[questionNumber].questionText,
+                quizBrain.getQuestionText(),
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -84,8 +82,9 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
+                //quizBrain.questionBank[questionNumber].questionAnswer=true;
                 bool correctAnswer =
-                    quizBrain.questionBank[questionNumber].questionAnswer;
+                    quizBrain.getQuestionAnswer();
 
                 if (correctAnswer == true) {
                   print('user got in right');
@@ -94,9 +93,8 @@ class _QuizPageState extends State<QuizPage> {
                 }
 
                 setState(() {
-                  questionNumber++;
+                  quizBrain.nextQuestion();
                 });
-                print(questionNumber);
               },
             ),
           ),
@@ -115,7 +113,7 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 bool correctAnswer =
-                    quizBrain.questionBank[questionNumber].questionAnswer;
+                    quizBrain.getQuestionAnswer();
 
                 if (correctAnswer == false) {
                   print('user got in right');
@@ -124,9 +122,9 @@ class _QuizPageState extends State<QuizPage> {
                 }
 
                 setState(() {
-                  questionNumber++;
+                  quizBrain.nextQuestion();
                 });
-                print(questionNumber);
+                
               },
             ),
           ),
