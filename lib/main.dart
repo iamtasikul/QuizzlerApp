3bb2465075ee1015daa6f26839647ff09baee1ab
Tasikul Islam <iamtasikul@gmail.com>
2020-12-31 11:17:01 +0530

    Abstraction in Action

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'question.dart';
+import 'quiz_brain.dart';
+
+QuizBrain quizBrain = new QuizBrain();
 
 void main() => runApp(Quizzler());
 
@@ -44,12 +46,6 @@ class _QuizPageState extends State<QuizPage> {
   // Question q1 =
   //     Question(q: 'You can lead a cow down stairs but not up stairs', a: false);
 
-  List<Question> questionBank=[
-    Question(q:'You can lead a cow down stairs but not up stairs.',a: false ),
-    Question(q:'Approximately one quarter of human bones are in the feet.' ,a: true),
-    Question(q:'A slug\'s blood is green.' ,a: true),
-  ];
-
   int questionNumber = 0;
 
   @override
@@ -64,7 +60,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                questionBank[questionNumber].questionText,
+                quizBrain.questionBank[questionNumber].questionText,
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -88,7 +84,8 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                bool correctAnswer = questionBank[questionNumber].questionAnswer;
+                bool correctAnswer =
+                    quizBrain.questionBank[questionNumber].questionAnswer;
 
                 if (correctAnswer == true) {
                   print('user got in right');
@@ -117,7 +114,8 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                bool correctAnswer = questionBank[questionNumber].questionAnswer;
+                bool correctAnswer =
+                    quizBrain.questionBank[questionNumber].questionAnswer;
 
                 if (correctAnswer == false) {
                   print('user got in right');
